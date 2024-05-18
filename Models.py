c8159f2410731de374e20df11e868e60e1ddd804
nchore <nchoreanthony@gmail.com>
2024-05-18 13:18:31 +0300

    added tables

--- Models.py
+++ Models.py
@@ -13,6 +13,8 @@ class Users(db.Model):
     role = db.Column(db.String(50), nullable=True)
     phone_number = db.Column(db.String(15), unique=True, nullable=False)
 
+    orders = db.relationship("Orders",backref = "user", lazy = True)
+
 class Products(db.Model):
     __tablename__ = 'products'
 
@@ -26,10 +28,15 @@ class Products(db.Model):
     created_at = db.Column(db.TIMESTAMP, server_default=db.func.now())
     updated_at = db.Column(db.TIMESTAMP, onupdate=db.func.now())
 
+    orders = db.relationship("Orders",backref = "product", lazy = True)
+
 class Orders(db.Model):
     __tablename__ = 'orders'
 
     id = db.Column(db.Integer, primary_key=True)  
-    product_id = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, nullable=False) 
+    product_id = db.Column(db.Integer, db.ForeignKey("products.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False) 
     placed_at = db.Column(db.TIMESTAMP, server_default=db.func.now())
+
+    product = db.relationship("Products", backref=db.backref("order_items", lazy = True))
+    user = db.relationship("Users", backref=db.backref("order_items", lazy = True))
