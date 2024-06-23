50a4d0f7d05199d93bf0be28985f8b7d2ad52aa3
nchore <nchoreanthony@gmail.com>
2024-06-23 12:43:38 +0300

    upgrade post

---
+++ migrations/versions/1df1fca41b31_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 1df1fca41b31
+Revises: dd44b1b8709b
+Create Date: 2024-06-23 12:41:33.927063
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1df1fca41b31'
+down_revision = 'dd44b1b8709b'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
